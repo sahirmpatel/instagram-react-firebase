be7fd7c97454397d8caeccfdabf37433644bce31
Sahir Patel <sahirmpatel@gmail.com>
2022-01-01 18:57:30 +0530

    login page styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
@@ -17,7 +18,70 @@ const Login = () => {
     document.title = "Login - Instagram";
   }, []);
 
-  return <div className="">Login Page</div>;
+  // 1:47:
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="images\iphone-with-profile.jpg" alt="iPhone image" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div
+          className="flex flex-col items-center bg-white p-4
+        border border-gray-primary mb-4 rounded"
+        >
+          <h1
+            className="flex justify-center w-full 
+     "
+          >
+            <img
+              src="\images\logo.png"
+              alt="instagram logo"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email"
+              type="text"
+              placeholder="Email"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your Password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+                isInvalid && `opacity-50`
+              }`}
+            >
+              Log In
+            </button>
+          </form>
+
+          {error && (
+            <p className="mb-4 text-xs text-red-primary m-auto">{error}</p>
+          )}
+        </div>
+
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+          <p className="text-sm">
+            Dont have an account?{` `}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fill: (theme) => ({
+      red: theme("colors.red.primary"),
+    }),
+    colors: {
+      white: "#ffffff",
+      blue: {
+        medium: "#005c98",
+      },
+      black: {
+        light: "#262626",
+        faded: "#00000059",
+      },
+      gray: {
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
+      },
+      red: {
+        primary: "#ed4956",
+      },
+    },
     extend: {},
   },
   plugins: [],
